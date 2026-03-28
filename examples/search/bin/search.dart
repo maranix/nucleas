9493117263683a0aaf194b1a3598bb2ede6417d9
maranix <ramanverma4183@gmail.com>
2026-03-29 02:03:08 +0530

    feat: introduce asynchronous Resource primitive
    - Added a new reactive primitive Resource for handling asynchronous computations within the reactive graph.
    - Implemented ResourceState classes (ResourceLoading, ResourceLoaded, ResourceError) to track the computation lifecycle.
    - Exported Resource and state classes in lib/valence.dart.
    - Created a new examples/search CLI application demonstrating how to use Resource with package:http.

---
+++ examples/search/bin/search.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:valence/valence.dart';
+
+const baseURL = "dummyjson.com";
+
+final queries = <String>["phone", "mobile", "watch", "car", "table", "bottle"];
+
+final client = http.Client();
+
+Future<int> _searchProducts(String query) async {
+  final url = Uri.https(baseURL, "/products/search", {"q": query});
+  final res = await client.get(url);
+
+  if (res.statusCode != HttpStatus.ok) {
+    throw Exception("Blud this broke with ${res.statusCode}");
+  }
+
+  final json = jsonDecode(res.body) as Map<String, dynamic>;
+  return json["total"];
+}
+
+void main() async {
+  final searchQuery = store("");
+
+  final searchResource = resource(() {
+    final q = searchQuery();
+    return _searchProducts(q);
+  });
+
+  reactor(() {
+    // Reading both the Resource and the Store in the same Observer
+    final currentQuery = searchQuery();
+    if (currentQuery.isEmpty) return;
+
+    final searchState = searchResource();
+
+    final message = switch (searchState) {
+      ResourceLoading(:final data) =>
+        data != null
+            ? "Searching for '$currentQuery' (Keeping stale data: $data)..."
+            : "Searching for '$currentQuery'...",
+      ResourceLoaded(:final data) => "Got $data results for '$currentQuery'",
+      ResourceError(:final error) =>
+        "Failed to get results for '$currentQuery': $error",
+    };
+
+    print(message);
+  });
+
+  final stream = Stream.periodic(.new(seconds: 3), (_) {
+    if (queries.isEmpty) return;
+
+    searchQuery.dispatch(.run(handler: (_) => queries.removeLast()));
+  });
+
+  await stream.forEach((_) {});
+
+  client.close();
+}
--- packages/valence/lib/valence.dart
+++ packages/valence/lib/valence.dart
@@ -6,3 +6,11 @@ export 'src/primitive/batch.dart' show Batch, batch;
 export 'src/primitive/derive.dart' show Derive, derive;
 export 'src/primitive/reactor.dart' show Reactor, reactor;
 export 'src/primitive/store.dart' show Store, store;
+export 'src/primitive/resource.dart'
+    show
+        Resource,
+        ResourceState,
+        ResourceLoading,
+        ResourceLoaded,
+        ResourceError,
+        resource;
