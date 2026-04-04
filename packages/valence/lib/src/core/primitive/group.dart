1631811235038b5568e2802e267cdda0ba4d253d
maranix <ramanverma4183@gmail.com>
2026-04-04 11:22:39 +0530

    refactor(batch): rename batch -> group

--- packages/valence/lib/src/core/primitive/group.dart
+++ packages/valence/lib/src/core/primitive/group.dart
@@ -1,7 +1,7 @@
 import 'package:valence/src/constants.dart';
 import 'package:valence/src/core/scope.dart';
 
-void batch(void Function() fn, {Scope? scope}) {
+void group(void Function() fn, {Scope? scope}) {
   final s = scope ?? rootScope;
   s.scheduler.batch(fn);
 }
